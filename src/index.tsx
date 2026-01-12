1bc7cd1400046b39e2644011b15618fd2851433b
Kongtup <ttansiri524@gmail.com>
2026-01-12 22:52:31 +0700

    chores: draft project structure

---
+++ src/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import Homepage from "./pages/Homepage.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./assets/css/global.css";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Homepage />,
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(console.log);
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../assets/css/homepage.css";
+
+function Homepage() {
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
+
+export default Homepage;
